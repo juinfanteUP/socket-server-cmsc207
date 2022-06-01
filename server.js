e811a246542debe4a4a0a799e1e3ff6902bb19c9
erickpauljaucian <62129755+erickpauljaucian@users.noreply.github.com>
2022-06-02 00:34:49 +0800

    Revert port to 5000

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 5000;
 
 //Run server
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
